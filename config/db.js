2501bb224e738cb2ad831335c35769b6824aabe9
Sylvain ANTON <sylvainanton77@gmail.com>
2026-07-05 13:56:06 +0200

    tp2 configure la base de donnees sqlite

---
+++ config/db.js
@@ -0,0 +1,17 @@
+import Database from "better-sqlite3";
+
+const db = new Database("database.db");
+
+db.pragma("foreign_keys = ON");
+
+db.prepare(
+  `
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password_hash TEXT
+  )
+`,
+).run();
+
+export default db;
