765e8b32b6b6f3c50ce0a7cc04945b50c287c706
Sylvain ANTON <sylvainanton77@gmail.com>
2026-07-23 15:15:57 +0200

    tp4 branche helmet et les routes 2FA dans le serveur

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-// Charge les variables d'environnement AVANT tout autre import,
-// car routes/auth.js et middlewares/authCheck.js lisent process.env
-// dès leur évaluation (à l'import).
 import "dotenv/config";
-
 import express from "express";
+import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.js";
 import apiAuthRoutes from "./routes/apiAuth.js";
 import userRoutes from "./routes/user.js";
+import twoFactorRoutes from "./routes/twoFactor.js";
 import batcomputerRoutes from "./routes/batcomputer.js";
 import "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+        "upgrade-insecure-requests": null,
+      },
+    },
+  }),
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,6 +32,7 @@ app.use(express.static("public"));
 app.use("/auth", authRoutes);
 app.use("/api/auth", apiAuthRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api", twoFactorRoutes);
 app.use(batcomputerRoutes);
 
 app.listen(PORT, () => {
